f82babed7ef489828b1d5b0827a6f8249c16a4a4
Michael Diolosa <michael.diolosa@gmail.com>
2014-05-02 00:09:05 -0400

    Refactor into class

--- app.js
+++ app.js
@@ -1,123 +1,156 @@
 #!/usr/bin/env node --harmony
+/* jshint node: true, evil: true */
 
 'use strict';
 
-let program = require('commander');
-let packagejs = require('./package.json');
+var program = require('commander');
+var packagejs = require('./package.json');
 
 program.version(packagejs.version)
   .option('-i, --in [path]', 'directory where twitter JSON resides')
   .option('-o, --out [path]', 'directory where markdown files will be saved')
   .parse(process.argv);
 
-let dir = program.in;
-let out = program.out;
+var dirIn = program.in;
+var dirOut = program.out;
 
-if (!dir || !out) {
+if (!dirIn || !dirOut) {
   console.log('\n  Error: You must specify the in and out parameters');
   program.help();
 }
 
-let fs = require('fs');
-let path = require('path');
-let mkdirp = require('mkdirp');
-let _ = require('lodash');
-let moment = require('moment');
-let Grailbird = { data: {} };
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var _ = require('lodash');
+var moment = require('moment');
+var Grailbird = { data: {} };
 
-function escapeMarkdown(text) {
+var TweetDown = function TweetDown(inDir, outDir) {
+  this.inDir = inDir;
+  this.outDir = outDir;
+};
+
+TweetDown.prototype.init = function init() {
+  this.getFileList();
+  this.evaluateFiles();
+};
+
+TweetDown.prototype.getFileList = function getFileList() {
+  this.fileList = fs.readdirSync(this.inDir);
+};
+
+TweetDown.prototype.evaluateFiles = function evaluateFiles() {
+  this.fileList.map((function fileListMap(fileName) {
+    var filePath = path.join(this.inDir, fileName);
+    var fileData = fs.readFileSync(filePath, { encoding: 'utf8' });
+    eval(fileData);
+  }).bind(this));
+};
+
+TweetDown.prototype.writeFile = function writeFile(tweet, text) {
+  var createdAt = new Date(tweet.created_at);
+  var momDate = moment(createdAt);
+  var dateString = momDate.format('YYYYMMDDHHmmss');
+  var yearString = momDate.format('YYYY');
+  var monthString = momDate.format('MM');
+  
+  var outPath = path.join(this.outDir, yearString, monthString);
+  mkdirp.sync(outPath);
+  
+  var outFilePath = path.join(outPath, dateString + '.md');
+
+  fs.writeFileSync(outFilePath, text, { encoding: 'utf8' });
+};
+
+TweetDown.prototype.escape = function escapeMarkdown(text) {
   return text.replace(/([\\_\-*+.!`{}()\[\]#])/g, '\\$1');
-}
+};
 
-let fileList = fs.readdirSync(dir);
+TweetDown.prototype.processFile = function processFile(tweet) {
+  var origTweet = tweet;
+  var rt = '';
 
-for (let m = 0, n = fileList.length; m < n; m++) {
-  let filePath = path.join(dir, fileList[m]);
-  let fileData = fs.readFileSync(filePath, { encoding: 'utf8' });
-  eval(fileData.toString());
-}
-
-for(let key in Grailbird.data) {
-  let monthData = Grailbird.data[key];
-
-  for (let i = 0, j = monthData.length; i < j; i++) {
-    let tweet = monthData[i];
-    let origTweet = tweet;
-    let rt = '';
-
-    if (tweet.retweeted_status) {
-      tweet = tweet.retweeted_status;
-      rt = '[@' + escapeMarkdown(origTweet.user.screen_name) + '](http://twitter.com/' + origTweet.user.screen_name + '): RT ';
-    }
-
-    let text = tweet.text;
-
-    let createdAt = new Date(tweet.created_at);
-
-    // Generate links from entities
-    if (tweet.entities) {
-      let offset = 0;
-
-      let userMentions = tweet.entities.user_mentions || [];
-      let hashTags = tweet.entities.hashtags || [];
-      let media = tweet.entities.media || [];
-      let urls = tweet.entities.urls || [];
-
-      let entities = userMentions.concat(hashTags).concat(media).concat(urls);
-      entities = entities.sort(function (a, b) {
-        return a.indices[0] - b.indices[0];
-      });
-
-      let previousIndex = 0;
-      let split = [];
-
-      entities.map(function (entity) {
-        split.push({ text: escapeMarkdown(tweet.text.substring(previousIndex, entity.indices[0])) });
-        split.push({ entity: entity });
-        previousIndex = entity.indices[1];
-      });
-
-      split.push({ text: escapeMarkdown(tweet.text.substring(previousIndex)) });
-
-      text = '';
-      split.map(function (val) {
-        if (val.entity) {
-          let entity = val.entity;
-          if (entity.screen_name) {
-            text += '[@' + escapeMarkdown(entity.screen_name) + '](http://twitter.com/' + entity.screen_name + ')';
-          } else if (entity.text) {
-            text += '[' + escapeMarkdown('#' + entity.text) + '](http://twitter.com/search?q=%23' + entity.text + ')';
-          } else if (entity.sizes) {
-            text += '[' + escapeMarkdown(entity.display_url) + '](' + entity.media_url + ')';
-          } else if (entity.display_url) {
-            text += '[' + escapeMarkdown(entity.display_url) + '](' + entity.expanded_url + ')';
-          }
-        } else {
-          text += val.text;
-        }
-      });
-    }
-
-    text = '> ' + rt + '[@' + escapeMarkdown(tweet.user.screen_name) + '](http://twitter.com/' + tweet.user.screen_name + '): ' + text;
-
-    text = text.replace(/\n/g, '\n> ') + '\n';
-
-    if (tweet.geo.type) {
-      if (tweet.geo.type.toLowerCase() === 'point') {
-        text += '\nLocation: [' + escapeMarkdown(tweet.geo.coordinates[0] + ', ' + tweet.geo.coordinates[1]) + '](https://www.google.com/maps/@' + tweet.geo.coordinates[0] + ',' + tweet.geo.coordinates[1] + ',13z)';
-      }
-    }
-
-    text += '\nCreated At: ' + escapeMarkdown(createdAt.toString());
-
-    let momDate = moment(createdAt);
-    let dateString = momDate.format('YYYYMMDDHHmmss');
-    let yearString = momDate.format('YYYY');
-    let monthString = momDate.format('MM');
-    let outDir = path.join(out, yearString, monthString);
-    mkdirp.sync(outDir);
-    let outFilePath = path.join(outDir, dateString + '.md');
-
-    fs.writeFileSync(outFilePath, text, { encoding: 'utf-8' });
+  if (tweet.retweeted_status) {
+    tweet = tweet.retweeted_status;
+    rt = '[@' + this.escape(origTweet.user.screen_name) + '](http://twitter.com/' + origTweet.user.screen_name + '): RT ';
   }
-}
+
+  var text = tweet.text;
+
+  var createdAt = new Date(tweet.created_at);
+
+  // Generate links from entities
+  if (tweet.entities) {
+    var offset = 0;
+
+    var userMentions = tweet.entities.user_mentions || [];
+    var hashTags = tweet.entities.hashtags || [];
+    var media = tweet.entities.media || [];
+    var urls = tweet.entities.urls || [];
+
+    var entities = userMentions.concat(hashTags).concat(media).concat(urls);
+    entities = entities.sort(function (a, b) {
+      return a.indices[0] - b.indices[0];
+    });
+
+    var previousIndex = 0;
+    var split = [];
+
+    entities.map((function (entity) {
+      split.push({ text: this.escape(tweet.text.substring(previousIndex, entity.indices[0])) });
+      split.push({ entity: entity });
+      previousIndex = entity.indices[1];
+    }).bind(this));
+
+    split.push({ text: this.escape(tweet.text.substring(previousIndex)) });
+
+    text = '';
+    split.map((function (val) {
+      if (val.entity) {
+        var entity = val.entity;
+        if (entity.screen_name) {
+          text += '[@' + this.escape(entity.screen_name) + '](http://twitter.com/' + entity.screen_name + ')';
+        } else if (entity.text) {
+          text += '[' + this.escape('#' + entity.text) + '](http://twitter.com/search?q=%23' + entity.text + ')';
+        } else if (entity.sizes) {
+          text += '[' + this.escape(entity.display_url) + '](' + entity.media_url + ')';
+        } else if (entity.display_url) {
+          text += '[' + this.escape(entity.display_url) + '](' + entity.expanded_url + ')';
+        }
+      } else {
+        text += val.text;
+      }
+    }).bind(this));
+  }
+
+  text = '> ' + rt + '[@' + this.escape(tweet.user.screen_name) + '](http://twitter.com/' + tweet.user.screen_name + '): ' + text;
+
+  text = text.replace(/\n/g, '\n> ') + '\n';
+
+  if (tweet.geo.type) {
+    if (tweet.geo.type.toLowerCase() === 'point') {
+      text += '\nLocation: [' + this.escape(tweet.geo.coordinates[0] + ', ' + tweet.geo.coordinates[1]) + '](https://www.google.com/maps/@' + tweet.geo.coordinates[0] + ',' + tweet.geo.coordinates[1] + ',13z)';
+    }
+  }
+
+  text += '\nCreated At: ' + this.escape(createdAt.toString());
+
+  return text;
+};
+
+TweetDown.prototype.process = function proc() {
+  function monthDataMap(tweet) {
+    var text = tweetDown.processFile(tweet);
+    tweetDown.writeFile(tweet, text);
+  }
+
+  for (var key in Grailbird.data) {
+    var monthData = Grailbird.data[key];
+    monthData.map(monthDataMap);
+  }
+};
+
+var tweetDown = new TweetDown(dirIn, dirOut);
+tweetDown.init();
+tweetDown.process();
